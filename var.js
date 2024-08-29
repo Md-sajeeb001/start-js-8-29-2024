05983a8891ed25dbc21b5bd03af7a999dbb4c2ae
Md-sajeeb001 <mohammadsajeeb890@email.com>
2024-08-29 21:18:11 +0800

    variable

--- var.js
+++ var.js
@@ -3,8 +3,14 @@
 var personName = "SAJEEB";
 
 // using operator in js
-
 // +, -, *, /, %,
+// isNaN --> 
+var num = 23;
+var num2 = "shdbfsbn";
+// var resul = num + num2;
+console.log(num + parseInt(num2));
+// its give u an isNaN becouse num2 is string not a number
+//  
 
 // logical operator 
 // 1. logical and &&
@@ -15,3 +21,14 @@ var personName = "SAJEEB";
 // 1. "string"
 // 2. number
 // 3.boolean
+
+// compersion operator 
+// 1. getar than > 
+// 2. less than <
+// 3. getar than or equal >=
+// 4. less than or equal <=
+// 5. ==
+// 6. ===
+// 7. !==
+
+// 
