f8996b13bdc51f12eb1ab33f4e45b0fc5ec95859
Md-sajeeb001 <mohammadsajeeb890@email.com>
2024-08-29 21:07:32 +0800

    variable

---
+++ var.js
@@ -0,0 +1,17 @@
+// declear variable need 5 items in js *
+
+var personName = "SAJEEB";
+
+// using operator in js
+
+// +, -, *, /, %,
+
+// logical operator 
+// 1. logical and &&
+// 2. logical or ||
+// 3, logical not !
+
+// javascript a 3 data type thake
+// 1. "string"
+// 2. number
+// 3.boolean
